0d96d0333068b6e695ce14d522968ab3ef53c708
MANISH SINGH <120253074+menuubaba@users.noreply.github.com>
2024-08-11 23:55:52 +0530

    Add files via upload

---
+++ 8th day IF ELSE.py
@@ -0,0 +1,52 @@
+# If Else
+'''Define what to do next '''
+
+age= int(input("Enter your age : "))
+print("Your age is ",age)
+if (age>18):    
+    print("Your are Eligible to vote") # space is called 'INDENTATION' It's inbuild 
+else:
+    print("You can't Vote")
+print("yeh...")
+
+''' here if mean some condition agar aisa ho to ye karne hai  
+'''
+
+''' 
+Conditinal operators 53
+they are >,<,=>=,<=,==
+
+
+print(age>18)
+print(age<18)
+print(age>=18)
+print(age<=18)
+print(age==18)
+'''
+
+
+# another Example
+
+apple=int(input("Enter your apple : "))
+budget =200
+if (apple<=200):
+    print("Alexa Buy an Apple ")
+else:
+    print("Alexa don't buy Apple")
+    
+    
+    
+    
+#Else IF
+
+num = int(input("Enter the value of num: "))
+if (num < 0):
+  print("Number is negative.")
+elif (num == 0):
+  print("Number is Zero.")
+elif (num == 999):
+  print("Number is Special.")
+else:45
+  print("Number is positive.")
+
+print("I am happy now")
