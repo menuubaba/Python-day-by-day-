4ca574463181af31e7feab58c4cdcaa5be3eabcd
MANISH SINGH <120253074+menuubaba@users.noreply.github.com>
2024-08-14 23:31:51 +0530

    Add files via upload

---
+++ 10 th day While loop.py
@@ -0,0 +1,40 @@
+# WHILE LOOP 
+'''A while loop repeatedly executes a block of code 
+as long as a given condition remains true. 
+It's useful for tasks with unknown iteration count.
+ Syntax: while condition: code to be repeated
+'''
+count = 5
+while (count > 0):
+  print(count)
+  count = count - 1
+  
+  
+  # Else with while Loop 
+  x = 5
+while (x > 0):
+    print(x)
+    x = x - 1
+else:
+    print('counter is 0')
+    
+    
+    
+    i = int(input("Enter the number: "))
+print(i)
+while(i<=38):
+  i = int(input("Enter the number: "))
+  print(i)
+
+print("Done with the loop")
+
+# count = 5
+# while (count > 0):
+#   print(count)
+#   count = count - 1
+# else:
+#   print("I am inside else")
+
+# do {
+  # loop body;
+# }while(condition);
